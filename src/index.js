cdd7eb18a6bbb80d002d42bcca81252644f5b88c
Iryna Sadychenko <108194331+Irynasad@users.noreply.github.com>
2022-12-22 21:28:29 +0200

    update index.html

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
 import Notiflix from 'notiflix';
 import 'notiflix/dist/notiflix-3.2.5.min.css';
+
+const getItemTemplait = () => {
+  `<div class="photo-card">
+  <img src="" alt="" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+    </p>
+  </div>
+</div>`;
+};
