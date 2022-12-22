37c4c8bcd5915e0aea0fcd6b7dfc1c6a2f0ef615
Iryna Sadychenko <108194331+Irynasad@users.noreply.github.com>
2022-12-22 22:17:32 +0200

    update task

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Notiflix from 'notiflix';
 import 'notiflix/dist/notiflix-3.2.5.min.css';
+const axios = require('axios').default;
 
 const getItemTemplait = () => {
   `<div class="photo-card">
