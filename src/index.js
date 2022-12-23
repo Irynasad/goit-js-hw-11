7b69012fca66991238a2a865ca88de4d0e988971
Iryna Sadychenko <108194331+Irynasad@users.noreply.github.com>
2022-12-23 23:04:40 +0200

    update task

--- src/index.js
+++ src/index.js
@@ -1,23 +1,46 @@
 import Notiflix from 'notiflix';
 import 'notiflix/dist/notiflix-3.2.5.min.css';
-const axios = require('axios').default;
+import axios from 'axios';
+// const key = 32306530 - bed9e4ec976e510d33adb63ac;
+// axios.defaults.baseURL = 'https://pixabay.com/api/';
+const URL = 'https://pixabay.com/api/';
 
-const getItemTemplait = () => {
-  `<div class="photo-card">
-  <img src="" alt="" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes</b>
-    </p>
-    <p class="info-item">
-      <b>Views</b>
-    </p>
-    <p class="info-item">
-      <b>Comments</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads</b>
-    </p>
-  </div>
-</div>`;
-};
+// const axios = require('axios').default;
+
+function fetchImgs() {
+  return axios
+    .get(
+      '$URL?key=32306530 - bed9e4ec976e510d33adb63ac&q=yellow+flowers&image_type=photo'
+    )
+    .then(console.log);
+}
+fetchImgs();
+
+function onfetch() {
+  fetch(
+    '$URL?key=32306530 - bed9e4ec976e510d33adb63ac&q=yellow+flowers&image_type=photo'
+  )
+    .then(r => r.json())
+    .then(console.log);
+}
+onfetch();
+
+// const getItemTemplait = () => {
+//   `<div class="photo-card">
+//   <img src="" alt="" loading="lazy" />
+//   <div class="info">
+//     <p class="info-item">
+//       <b>Likes</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Views</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Comments</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Downloads</b>
+//     </p>
+//   </div>
+// </div>`;
+// };
